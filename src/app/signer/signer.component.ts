2bb3a33ddf8525b3c4de89f386e013e3a17ffbc1
Danilo Bispo <danilobispogalvao@gmail.com>
2022-06-07 09:21:30 -0300

    Remaining code from last commit

--- src/app/signer/signer.component.ts
+++ src/app/signer/signer.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import LacunaSignerWidget from 'lacuna-signer-widget';
 import { Receita } from '../model/receita';
 import { Usuario } from '../model/usuario';
 
@@ -18,25 +20,52 @@ export class SignerComponent implements OnInit {
 
   fileName = '';
 
-  isChecked : Boolean = false
 
-  receitaSample = new Receita("Fulano de tal", ['Ibuprofeno', 'Rivotril', 'Omeprazol', 'Benegrip'])
+  isChecked: Boolean = false
+  disableForms: Boolean = false
 
-  embedSignForm = new FormGroup({
-    cpf: new FormControl(''),
-    email: new FormControl(''),
-    nome: new FormControl('')
+  receita = new Receita("Fulano de tal", ['Ibuprofeno', 'Rivotril', 'Omeprazol', 'Benegrip'])
+  usuario = new Usuario("", "", "");
+
+  embedSignForm = new UntypedFormGroup({
+    cpf: new UntypedFormControl(''),
+    email: new UntypedFormControl(''),
+    nome: new UntypedFormControl('')
   });
 
-  updateProfile(){
-    alert(this.embedSignForm.get('cpf')?.value)
+  updateProfile() {
+    console.log("Nome: ", this.usuario.nome);
+    console.log("email: ", this.usuario.email)
+    console.log("cpf: ", this.usuario.cpf);
   }
 
-  updateCertificadoDeTeste(){
-
+  updateCertificadoDeTeste() {
   }
 
-  constructor() { }
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      const headers: HttpHeaders = new HttpHeaders({
+          'Content-Type': 'undefined',
+          'x-api-key': 'API Sample App|43fc0da834e48b4b840fd6e8c37196cf29f919e5daedba0f1a5ec17406c13a99'
+      });
+      this.fileName = file.name;
+      const formData = new FormData();
+      formData.append("file", file);
+      const upload$ = this.http.post("https://dropsigner.com/api/uploads", formData, {headers: headers});
+      upload$.subscribe();
+    }
+  }
+
+  renderWidget(){
+    this.disableForms = true;
+    var widget = new LacunaSignerWidget();
+    widget.render("", 'embed-container')
+  }
+
+  constructor(private http: HttpClient) {
+   }
 
   ngOnInit(): void {
   }
